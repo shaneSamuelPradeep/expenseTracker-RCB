55887c08eeb27c4593d61c64445f9cbd1f053a4b
Shane Samuel Pradeep <shane-samuel.pradeep@eleve.isep.fr>
2024-12-31 13:37:38 +0100

    Update ViewSpending.java

--- src/gui/ViewSpending.java
+++ src/gui/ViewSpending.java
@@ -1,10 +1,1 @@
-package gui;
 
-public class ViewSpending {
-
-    public void setVisible(boolean b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
-    }
-    
-}
